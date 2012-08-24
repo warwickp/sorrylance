9ddc7771ae98d607b32553e13a29b08a74b06755
Warwick Poole <wpoole@gmail.com>
2012-08-24 14:27:25 -0400

    working?

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>HowManyTourDeFranceWinsDoesLanceArmstrongHave.com</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body style="text-align: center; padding-top: 10px;">
+
+<a href="/" style="font-weight: 
+normal; font-size: 35pt; 
+font-family: Veranda, Helvetica, sans-serif; text-decoration:line-through; color: 
+black;" title="Was 7">7</a><a href="/" style="font-weight: 
+bold; font-size: 440pt; 
+font-family: Veranda, Helvetica, sans-serif; text-decoration: none; color: 
+black;" title="Now 0">0</a>
+</body>
+</html>
